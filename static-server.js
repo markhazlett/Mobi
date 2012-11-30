b2354c285d94c1ce58ce4f83420a5a4c24ab88db
Eric Kryski <e.kryski@gmail.com>
2012-11-30 12:26:49 -0700

    started setting up code for grunt and static nodejs server

---
+++ static-server.js
@@ -0,0 +1,21 @@
+var express = require("express"),
+    app = express(),
+    port = 8000;
+
+app.get("/", function(req, res) {
+  res.redirect("/index.html");
+});
+
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/src'));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  app.use(app.router);
+});
+
+app.listen(port);
+console.log('Static server listening on port: ' + port);
